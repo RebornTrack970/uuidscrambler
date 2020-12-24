08060514aa79a287d3cc33792263d83c97266c1e
RebornTrack970 <62565267+RebornTrack970@users.noreply.github.com>
2020-12-24 22:04:29 +0300

    Random name

--- scripts/main.js
+++ scripts/main.js
@@ -8,6 +8,8 @@ Events.on(Trigger.newGame.class,e=>{
 	var num7 = Mathf.round(Mathf.random() * 9).toString();
 	var num8 = Mathf.round(Mathf.random() * 9).toString();
 	var number = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8;
+	var name = "XxEbicPR0Gamer" + number +"xX";
 	var uuid = Packages.arc.util.serialization.Base64Coder.encodeString(number);
 	Core.settings.put("uuid", uuid);
+	Core.settings.put("name", name);
 })
